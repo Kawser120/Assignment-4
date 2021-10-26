5631d550a069d3f428cd6cacb45e5a6525fe0556
kawser120 <cse_2012020284@lus.ac.bd>
2021-10-26 13:42:40 +0600

    completed

---
+++ src/Kawser/Football.java
@@ -0,0 +1,19 @@
+package kawser;
+
+/*
+
+Name:Kawser Ahmed
+Id: 2012020284
+section: F
+Email:cse_2012020284@lus.ac.bd
+Date:12/09/2021
+
+ */
+
+public class Football extends Sports{
+    public Football(){
+        super();
+        System.out.println("Football class called");
+
+    }
+}
---
+++ src/Kawser/Sports.java
@@ -0,0 +1,16 @@
+package kawser;
+/*
+
+Name:Kawser Ahmed
+Id: 2012020284
+section: F
+Email:cse_2012020284@lus.ac.bd
+Date:12/09/2021
+
+ */
+public class Sports {
+
+    public Sports(){
+        System.out.println("Sports class called");
+    }
+}
